a5289ea1e998c1f2eb849758e7e0f6ddaeb6b1cb
Greg <gregory.mkv@gmail.com>
2018-10-21 13:36:30 -0400

    Rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-#![feature(nll)]
 use std::process::Command;
-
-fn run_command(command: &mut Command) -> String {
-    String::from_utf8_lossy(&command.output().unwrap().stdout).into_owned()
-}
 
 #[derive(Debug)]
 struct Monitor {
@@ -12,77 +7,87 @@ struct Monitor {
     name: String,
 }
 
-impl Default for Monitor {
-    fn default() -> Self {
-        Monitor {
-            width: 1920,
-            height: 1080,
-            name: "eDP1".to_string(),
-        }
-    }
-}
-
 impl Monitor {
     fn resolution(&self) -> String {
         format!("{}x{}", self.width, self.height)
     }
+
+    fn from_line(line: &str, next: Option<&str>) -> Option<Monitor> {
+        let name = line.split_whitespace().nth(0)?.to_string();
+        let mut resolution = next?.split_whitespace().nth(0)?.split("x");
+
+        return Some(Monitor {
+            name,
+            width: resolution.nth(0)?.parse().expect("ParseError"),
+            height: resolution.nth(0)?.parse().expect("ParseError"),
+        });
+    }
 }
 
-fn get_secondary_monitor() -> Option<Monitor> {
-    let monitors = run_command(Command::new("xrandr").arg("-q"));
-    let mut lines = monitors.lines();
+fn run_command(command: &mut Command) -> String {
+    String::from_utf8_lossy(&command.output().unwrap().stdout).into_owned()
+}
 
-    while let Some(line) = lines.next() {
-        if line.contains(" connected") && line.contains("HDMI") {
-            let monitor_name = line.split_whitespace().nth(0)?;
+struct Xrandr;
 
-            if let Some(resolution_line) = lines.next() {
-                let mut resolution =
-                    resolution_line.split_whitespace().nth(0)?.split("x");
+impl Xrandr {
+    fn is_monitor_line(str: &str) -> bool {
+        str.contains(" connected") 
+    }
 
-                return Some(Monitor {
-                    width: resolution.nth(0)?.parse().expect("ParseError"),
-                    height: resolution.nth(0)?.parse().expect("ParseError"),
-                    name: monitor_name.to_string(),
-                });
+    fn get_monitors() -> Vec<Monitor> {
+        let mut monitors = Vec::new();
+
+        let xrandr = run_command(Command::new("xrandr").arg("-q"));
+        let mut lines = xrandr.lines();
+
+        while let Some(line) = lines.next() {
+          if Self::is_monitor_line(line) {
+            if let Some(monitor) = Monitor::from_line(line, lines.next()) {
+              monitors.push(monitor);
+            }
+          }
+        }
+        monitors
+    }
+
+    fn setup() -> std::io::Result<()> {
+        let monitors = Self::get_monitors();
+        if monitors.len() == 0 {
+            return Ok(());
+        }
+
+        let mut cmd = Command::new("xrandr");
+        let primary = &monitors[0];
+
+        let total_width = monitors.iter().fold(-primary.width,|acc, m| acc + m.width);
+        let max_height = monitors.iter().fold(0,|acc, m| if m.height > acc { m.height } else { acc });
+        let mut non_primary_pos = 0;
+
+        for (i, monitor) in monitors.iter().enumerate() {
+            cmd
+                .arg("--output").arg(&monitor.name)
+                .arg("--mode").arg(&monitor.resolution())
+                .arg("--pos");
+
+            if i == 0 {
+                cmd
+                    .arg(format!("{}x{}", (total_width - primary.width)/2, max_height))
+                    .arg("--primary");
+            }
+            else {
+                cmd.arg(format!("{}x{}", non_primary_pos, (monitor.height - max_height)/2));
+                non_primary_pos += monitor.width;
             }
         }
+
+        cmd.spawn().map(|_r| ())
     }
-    return None;
 }
 
-fn main() {
-    let primary_monitor = Monitor::default();
-    let mut cmd = Command::new("xrandr");
 
-    if let Some(secondary_monitor) = get_secondary_monitor() {
-        let xrandr_args = format!(
-            "--output {} --mode {} --pos 0x0 --output {} --primary --mode {} \
-             --pos {}x{}",
-            secondary_monitor.name,
-            secondary_monitor.resolution(),
-            primary_monitor.name,
-            primary_monitor.resolution(),
-            (secondary_monitor.width - primary_monitor.width) / 2,
-            secondary_monitor.height
-        );
 
-        cmd.args(xrandr_args.split_whitespace());
-    }
-    else {
-        cmd.args(&["--output", &primary_monitor.name])
-            .args(&["--mode", &primary_monitor.resolution()])
-            .args(&["--pos", "0x0"])
-            .arg("--primary");
-    }
 
-    println!(
-        "{}",
-        match cmd.spawn() {
-            Ok(_res) => "Ok",
-            Err(_e) => "Error",
-        }
-    )
-
-    // println!("{:?}", randr);
+fn main() -> std::io::Result<()> {
+    Xrandr::setup()
 }
